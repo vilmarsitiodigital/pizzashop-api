9adeebce8fce27e68216bfcfd02e49eb1a04d31d
vilmarsitiodigital <vilmar@sitiodigital.com.br>
2024-04-15 08:50:24 -0300

    chore: update seed

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -55,7 +55,7 @@ const [manager] = await db
   .insert(users)
   .values({
     name: faker.person.fullName(),
-    email: 'vilmarbatista.dev@gmail.com',
+    email: 'demo@vilmarbatista.com.br',
     role: 'manager',
   })
   .returning()
